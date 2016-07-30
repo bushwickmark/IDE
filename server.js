6c4d546e4498a89d4e3d030a96ec58968d400db2
marktechchristensentech@gmail.com <marktechchristensentech@gmail.com>
2016-07-30 08:53:21 -0400

    test

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.use(express.static(__dirname + '/public'));
 
 //var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 //var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-
+//test
 
 app.get('/say/hello', sayHello);
 app.get('/api/users', function(req, res){
